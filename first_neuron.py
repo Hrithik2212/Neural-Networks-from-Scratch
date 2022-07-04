9ba0298b259c7dff0c2609dae58ccaf254893fec
Hrithik2212 <hr6466@srmist.edu.in>
2022-07-04 20:54:23 +0530

    first commit

---
+++ first_neuron.py
@@ -0,0 +1,30 @@
+
+from pip import main
+
+
+inputs = [1.2,5.1,2.1]
+weights = [3.1,2.1,8.7]
+
+bias = 3
+
+output = 0
+
+def simple_neuron(inp,w,b=0):
+    '''
+    Each Neuron has inputs and there are eqaul number of weights to the no of input and there  is 
+    also bias for each neuron
+    '''
+    out=0
+    try:
+        for i,j in zip(inp,w):
+            out += i*j
+            # print(out)
+        out += b
+        return out
+    except :
+        if len(inp) != len(w):
+            print("Lenght of Inputs and Weights are to be the same")
+           
+
+print(simple_neuron(inputs,weights,bias))
+
---
+++ multiple_neurons.py
@@ -0,0 +1,19 @@
+inputs = [1,2,3,2.5]
+
+weights1 = [2.1 ,3.4, 4.5 ,-1.2]
+weights2 = [1.4 ,-2.1, 3, 1.7]
+weights3 = [1.5 , -1 ,-0.6 , 2.3]
+
+bias1 = 3
+bias2 = 4
+bias3 = 5
+
+weights = [weights1,weights2,weights3]
+from pprint import pprint as pp
+pp(weights)
+# Modelling three neurons 
+from first_neuron import simple_neuron as single_neuron
+
+def multiple_neurons(inp,ws,b):
+    layer_out = [single_neuron(inp,x,y) for x,y in zip(ws,b)]
+
