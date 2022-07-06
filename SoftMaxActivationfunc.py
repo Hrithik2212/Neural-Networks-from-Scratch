cefc8573e20cf5b7fd96e2c0bbbff8814371fc92
Hrithik2212 <hr6466@srmist.edu.in>
2022-07-06 08:29:16 +0530

    SoftMax

---
+++ SoftMaxActivationfunc.py
@@ -0,0 +1,65 @@
+# Exponentation 
+import math
+import numpy as np
+E = math.e
+
+exp_values = []
+
+output_vals = [4.1,2.1,1.35]
+for i in output_vals:
+    exp_values.append(E**i)
+
+print(*exp_values)
+
+
+# Normalization
+
+norm_base = np.sum(exp_values,axis=0,keepdims=True)
+norm_output = [x/norm_base for x in exp_values]
+
+print(*norm_output)
+print(np.sum(norm_output))
+
+
+class Layer_Dense:
+    def __init__(self,n_inputs,n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs,n_neurons)
+        self.biases = np.zeros((1,n_neurons))
+    def forward(self,inputs):
+        self.output = np.dot(inputs , self.weights) + self.biases
+
+def spiral_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points)  # radius
+        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+
+X,y = spiral_data(100,3)
+
+class Activation_SoftMax():
+    def forward(self,inputs):
+        exp_values = np.exp(inputs - np.max(inputs,axis=1,keepdims=True))
+        probabilties = exp_values/np.sum(exp_values,axis=1,keepdims=True)
+        self.output = probabilties
+
+class Activation_ReLU:
+    def forward(self , inputs):
+        self.output = np.maximum(0,inputs)
+
+
+# Defining the Neural Networks
+dense1 = Layer_Dense(2,3)
+activation1 = Activation_ReLU()
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_SoftMax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+print(activation2.output)
